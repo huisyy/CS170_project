760a1bed3b5ef4a978ee85fc91a28d796ce9e508
huisyy <meganhu@berkeley.edu>
2021-12-02 18:28:21 -0800

    kermit

---
+++ helperFunctions.py
@@ -0,0 +1,55 @@
+import solver as sv
+import numpy as np
+import math
+
+idToObject = {}
+
+def initIDToObject(tasks):
+    global idToObject
+    for task in tasks:
+        idToObject[task.task_id] = task
+    
+# returns list of Task objects
+def chooseTasks(idToProb):
+    ids = []
+    probs = []
+    for id, prob in idToProb:
+        ids.append(id)
+        probs.append(prob)
+    # for id in idToProb:
+    #     ids.append(id)
+    # for prob in idToProb:
+    #     probs.append(prob)
+    tasks = np.random.choice(ids, len(ids)//2, replace = False, p=probs)
+
+    taskObjects = []
+    for id in tasks:
+        taskObjects.append(idToObject[id])
+    return taskObjects #a list of tasks
+
+def normalizeVector(dict):
+    normFactor = 1.0/sum(dict.values())
+    for key in dict:
+        dict[key] = dict[key]*normFactor
+
+    return dict
+
+def profitCalculator(taskObjects, time=0):
+    profit = 0
+    # time = 0
+    for task in taskObjects:
+        #if the task will go over the deadline, profit decays
+        if task.duration + time > task.deadline:
+            minutesLate = task.duration - task.deadline
+            profit += decayCalculator(task.perfect_benefit, minutesLate)
+            # print("HITS HERE ", task.task_id)
+        else:
+            profit += task.perfect_benefit
+        time += task.duration
+    return profit
+
+def decayCalculator(profit, minutesLate):
+    if minutesLate <= 0:
+        return profit
+    return profit * (math.e ** (-0.0170 * minutesLate))
+
--- solver.py
+++ solver.py
@@ -1,20 +1,118 @@
+from numpy.core.fromnumeric import choose
 from parse import read_input_file, write_output_file
 import os
+import math
+import numpy as np
+import helperFunctions as hF
 
+# solves
 def solve(tasks):
     """
     Args:
-        tasks: list[Task], list of igloos to polish
+        tasks [list of task Objects]: list[Task], list of igloos to polish 
     Returns:
         output: list of igloos in order of polishing  
     """
-    pass
+    # initializes global task ID to object dictionary
+    hF.initIDToObject(tasks)
+    
+    finalProfit, taskList = helper(0, tasks, [], 0)
+
+    return [task.task_id for task in taskList]
+
+# helper
+def helper(currTime, potentialTasks, currTasks, currProfit):
+    """
+    Args:
+        currTime[int] 
+        potentialTasks[list of task objects]
+        currTasks[list of task objects]
+        currProfit[integer]
+    Returns:
+        final profit, list of task objects  
+    """
+    if currTime == 1440:
+        return currProfit, currTasks
+    idToProb = scorer(currTime, potentialTasks)
+    #chooseTasks returns a dictionary of ids to probabilities
+    potentialTasks = hF.chooseTasks(idToProb)
+    if not potentialTasks: # if no potential tasks are available
+        return currProfit, currTasks
+    
+    maxProfit = -math.inf
+    chosenTasks = None
+    #chooseTasks returns a dictionary of ids to probabilities
+    for potentialTask in potentialTasks:
+        tasks_copy = tasks.copy()
+        tasks_copy.remove(potentialTask) 
+        potentialTaskProfit = hF.decayCalculator(potentialTask.perfect_benefit, potentialTask.duration + currTime - potentialTask.deadline)
+        newProfit, newTasks = helper(currTime + potentialTask.duration, tasks_copy, currTasks + [potentialTask], currProfit + potentialTaskProfit)
+        if newProfit > maxProfit:
+            maxProfit = newProfit
+            chosenTasks = newTasks
+
+    # returns profit, task
+    return maxProfit, currTasks + chosenTasks
 
 
-# Here's an example of how to run your solver.
-# if __name__ == '__main__':
-#     for input_path in os.listdir('inputs/'):
-#         output_path = 'outputs/' + input_path[:-3] + '.out'
-#         tasks = read_input_file(input_path)
-#         output = solve(tasks)
-#         write_output_file(output_path, output)
+
+
+#Reupdates the score for every task. Takes in the current time and possible tasks.
+
+def by_value(item):
+    return item[1]
+
+
+def scorer(time, tasks):
+    """
+    Args:
+        time[int]: the current time
+        tasks: list of Task objects
+    Returns:
+        dictionary mapping task IDs to their normalized score   
+    """
+    scores = {}
+    for task in tasks:
+        if time + task.duration > 1440:
+            continue
+        if time + task.duration > task.deadline:
+            score = task.perfect_benefit * (math.e ** (-.017*(time + task.duration - task.deadline)))
+        else:
+            score = task.perfect_benefit
+        
+        score = (score/task.duration) 
+        scores[task.task_id] = score
+    
+    if not scores: # if there are no possible tasks
+        return None
+    
+    #normalize the vector
+    normalized = hF.normalizeVector(scores)
+    sortedTasks = sorted(normalized.items(), key=by_value)
+    
+    return sortedTasks
+    
+
+# return a dictionary
+
+
+
+
+# Calculates the profits if tasks are done in this order
+
+
+
+# run_folders = ['large', 'medium', 'small']
+run_folders = ['small']
+
+if __name__ == '__main__':
+    for folder in run_folders:
+        # for i in range(1, 301):
+        for i in range(1, 2):
+            if folder == 'small' and i == 184:
+                continue
+            output_path = 'outputs/' + folder + '/' + folder + '-' + str(i) + '.out'
+            tasks = read_input_file('inputs/' + folder + '/' + folder + '-' + str(i) + '.in')
+            output = solve(tasks)
+            write_output_file(output_path, output)
+
